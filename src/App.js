82bdcab8b2821cb568a3ebda21570469f7de6fe3
mateusbra <suehtamzarb@gmail.com>
2022-11-27 14:29:29 -0300

    front

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Api from "./Api";
-import { Button } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
+import {Button,Toast} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchToken, onMessageListener } from './firebase';
+
+
+
+
+
 function App() {
   const [URL,setURL] = useState(null);
   const [isLoading,setIsLoading] = useState(true);
   const [mode,setMode] = useState(false);
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({title: '', body: ''});
+  const [isTokenFound, setTokenFound] = useState(false);
+  fetchToken(setTokenFound);
 
+  onMessageListener().then(payload => {
+    setNotification({title: payload.notification.title, body: payload.notification.body})
+    setShow(true);
+    console.log(payload);
+  }).catch(err => console.log('failed: ', err));
 
+  const onShowNotificationClicked = () => {
+    setNotification({title: "Notification", body: "This is a test notification"})
+    setShow(true);
+  }
 
-
+ 
+  
+      
+  
   const handleClick = async () => {
     await Api.setMode(!mode);
     setMode(!mode);
@@ -23,6 +45,7 @@ function App() {
       let bool_mode = await Api.getMode();
       setMode(bool_mode);
       setIsLoading(false);
+
   }
 
       fetchURLAndMode();
@@ -47,6 +70,31 @@ function App() {
     </div>
     </div>
     )}
+
+
+<Toast onClose={() => setShow(false)} show={show} delay={3000} autohide animation style={{
+          position: 'absolute',
+          top: 20,
+          right: 20,
+          minWidth: 200
+        }}>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded mr-2"
+              alt=""
+            />
+            <strong className="mr-auto">{notification.title}</strong>
+            <small>just now</small>
+          </Toast.Header>
+          <Toast.Body>{notification.body}</Toast.Body>
+        </Toast>
+      <header className="App-header">
+        {isTokenFound && <h1> Notification permission enabled 👍🏻 </h1>}
+        {!isTokenFound && <h1> Need notification permission ❗️ </h1>}
+        
+        <Button onClick={() => onShowNotificationClicked()}>Show Toast</Button>
+      </header>
     </>
   );
 }
